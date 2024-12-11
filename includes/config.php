b5d263277480843f95124162de0b00afbbc0c63a
4ng3l001 <30834068+4ng3l001@users.noreply.github.com>
2024-12-11 02:24:21 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -12,9 +12,15 @@
 |
 |
 */
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  //define( 'DB_HOST', 'localhost' );          // Set database host
+  //define( 'DB_USER', 'root' );             // Set database user
+  //define( 'DB_PASS', '' );             // Set database password
+  //define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+  define( 'DB_HOST', 'bdbe0gao4gmrine3smqc-mysql.services.clever-cloud.com' );          // Set database host
+  define( 'DB_USER', 'ukhoqhoeubh9ek6r' );             // Set database user
+  define( 'DB_PASS', '68IcpaSm2Ns6954F9Z9w' );             // Set database password
+  define( 'DB_NAME', 'bdbe0gao4gmrine3smqc' );        // Set database name
+
 
 ?>
