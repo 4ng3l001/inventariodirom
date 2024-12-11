de46ce9539d6548a32a8356f34c3e62754653e35
4ng3l001 <30834068+4ng3l001@users.noreply.github.com>
2024-12-11 02:35:40 -0500

    Update auth_v2.php

--- auth_v2.php
+++ auth_v2.php
@@ -16,10 +16,10 @@ $password = remove_junk($_POST['password']);
            updateLastLogIn($user['id']);
            // redirect user to group home page by user level
            if($user['user_level'] === '1'):
-             $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+             $session->msg("s", "Hello ".$user['username'].", Bienvenido a DIROM inventario");
              redirect('admin.php',false);
            elseif ($user['user_level'] === '2'):
-              $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+              $session->msg("s", "Hello ".$user['username'].", Bienvenido a DIROM inventario");
              redirect('special.php',false);
            else:
               $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
